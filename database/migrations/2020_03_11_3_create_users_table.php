f83f9820ff937ce38644c78538efab2a0ee8acb0
Alejandro Calixto Villavicencio <al221811773@gmail.com>
2020-03-10 22:27:28 -0600

    Migraciones y apis

--- database/migrations/2020_03_11_3_create_users_table.php
+++ database/migrations/2020_03_11_3_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('nombre', 100);
+            $table->string('apellido', 100);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('rol_id');
+
+            $table->foreign('rol_id')->references('id')->on('roles');
         });
     }
 
---
+++ database/migrations/2020_03_11_4_create_comentarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('comentario');
+            $table->integer('estrella');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('restaurante_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('restaurante_id')->references('id')->on('restaurantes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reseñas');
+    }
+}
